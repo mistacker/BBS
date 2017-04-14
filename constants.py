87e8f51f49834c9776a947ce382c3a00f9fb2df0
wangBc <2596279105@qq.com>
2017-04-14 17:51:29 +0800

    finish

--- constants.py
+++ constants.py
@@ -6,8 +6,8 @@ CAPTCHA = 'yongyupanduandeyanzhengma'
 FRONT_USER_TEL = 'thisisfrontstel'
 
 # 阿里大于的两个key
-appkey = '23584025'
-secret = 'de6149e7409f9085b5bb486656d1d5af'
+appkey = ''
+secret = ''
 
 # 七牛的两个key
 AccessKey = 'HjYzkVVwmfucfaLhJx7_95CR5YPDkdQOEMjmCn4u'
